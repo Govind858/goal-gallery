7e2a982b3a8419de9784ca8b18fb9dd3b5f360d1
govind858 <govindjayakumar858@gmail.com>
2025-05-26 12:06:22 +0530

    added google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V572XNG6QC"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V572XNG6QC');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
